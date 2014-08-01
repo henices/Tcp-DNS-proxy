640a2c707d7716d3e6ccde07bb10f15b0975269f
henices <zhouzhenster@gmail.com>
2014-08-01 10:03:37 +0800

    add install.sh

---
+++ install.sh
@@ -0,0 +1,22 @@
+#! /usr/bin/env bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+function command_exists {
+    hash "$1" 2>/dev/null
+}
+
+if ! command_exists pip; then
+    echo "Please install pip to run this script."
+    exit 1
+fi
+
+if ! command_exists git; then
+    echo "Please install git to run this script"
+fi
+
+echo -e ">>> Installing python moudules ...\n"
+pip install -r "$SCRIPT_DIR/requirements.txt"
+
+echo -e "\n>>> Pulling submodule source code, wait ...\n"
+git submodule update --init --recursive
