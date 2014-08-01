6a68d4aa0e1e628362196a61ae0244c2ebdcfd9b
henices <zhouzhenster@gmail.com>
2014-08-01 13:53:41 +0800

    update py2exe script

--- win/toexe.py
+++ win/toexe.py
@@ -1,14 +1,21 @@
 from distutils.core import setup
 import py2exe
+import sys
+sys.path.append('..')
+import third_party
+
+
 setup(
-    version = "",
+    version = "1.2",
     description = "tcp dns proxy",
     name = r"tcp dns proxy",
     options = {"py2exe": {"compressed": True,
                           "optimize": 2,
                           "ascii": 0,
+                          "dist_dir": ".",
                           "includes": [],
-                          "bundle_files": 1
+                          "bundle_files": 1,
+                          "dll_excludes": ['w9xpopen.exe']
                           }},
     zipfile = None,
     # targets to build
@@ -18,5 +25,5 @@ setup(
                     #(0,"1367526275_127248.ico"),
                     #(1,"1367526275_127248.ico")
                 ]
-            }],
+              }],
     )
