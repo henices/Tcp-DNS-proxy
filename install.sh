98d3db21985ffc7878106b60be50c57805674790
henices <zhouzhenster@gmail.com>
2015-12-30 14:36:41 +0800

    add more information

--- install.sh
+++ install.sh
@@ -13,6 +13,7 @@ fi
 
 if ! command_exists git; then
     echo "Please install git to run this script"
+    exit 1
 fi
 
 echo -e ">>> Installing python moudules ...\n"
@@ -20,3 +21,6 @@ pip install -r "$SCRIPT_DIR/requirements.txt"
 
 echo -e "\n>>> Pulling submodule source code, wait ...\n"
 git submodule update --init --recursive
+
+echo -e "\n [OK] please run sudo python tcpdns.py -f tcpdns.json.example"
+
